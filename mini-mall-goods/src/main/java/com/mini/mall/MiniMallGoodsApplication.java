c636160685fa3eb7e3102fb451f654a61517c395
CM_pipi <cmj068@163.com>
2020-03-21 22:29:33 +0800

    创建模块

---
+++ mini-mall-goods/src/main/java/com/mini/mall/MiniMallGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.mini.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMallGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniMallGoodsApplication.class, args);
+    }
+
+}
